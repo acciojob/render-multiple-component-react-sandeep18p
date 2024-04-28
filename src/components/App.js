56eab4c736a225f4109da8981d28aaec0535e5c2
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-28 18:50:24 +0530

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import React, {Component, useState} from "react";
+import Project from './Project';
 import '../styles/App.css';
+function App() {
+  const projects = [
+    {
+      name: "Project 1",
+      description: "Description of Project 1"
+    },
+    {
+      name: "Project 2",
+      description: "Description of Project 2"
+    },
+    {
+      name: "Project 3",
+      description: "Description of Project 3"
+    },
+    {
+      name: "Project 4",
+      description: "Description of Project 4"
+    }
+    
+  ];
 
-const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div className="ns-wrapper">
+      {projects.map((project, index) => (
+        <Project
+          key={index}
+          name={project.name}
+          description={project.description}
+        />
+      ))}
+    </div>
+  );
 }
 
-
 export default App;
+
